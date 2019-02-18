6ca5d7b03fdaacc1191b530fab07a2cae620c70f
PGrantcharov <peter.g@columbia.edu>
2019-02-17 22:27:43 -0800

    updated diagram and sql translation

--- General/ER_translate_SQL_po.sql
+++ General/ER_translate_SQL_po.sql
@@ -31,7 +31,8 @@ CREATE TABLE arena (
 
 CREATE TABLE users (
 	u_id int PRIMARY KEY,
-	name text NOT NULL
+	name text NOT NULL,
+	password NOT NULL,
 );
 
 
@@ -47,9 +48,16 @@ CREATE TABLE sportsbook (
 );
 
 
+-- sportsbook table
+CREATE TABLE bet_type (
+	bt_id int PRIMARY KEY,
+	name text NOT NULL
+);
+
+
 CREATE TABLE game(
 	g_id int PRIMARY KEY,
-    game_time timestamp NOT NULL,
+  game_time timestamp NOT NULL,
 	a_id int REFERENCES arena (a_id),
 	t_id_home int REFERENCES team (t_id),
 	t_id_away int REFERENCES team (t_id),
@@ -62,8 +70,8 @@ CREATE TABLE game(
 	away_q2_score int NOT NULL,
 	away_q3_score int NOT NULL,
 	away_q4_score int NOT NULL,
-	home_ot_score int NOT NULL,
-	away_ot_score int NOT NULL,
+	home_ot_score int,
+	away_ot_score int,
 	CHECK (
 		home_q1_score >= 0 AND
 		home_q2_score >= 0 AND
@@ -73,118 +81,109 @@ CREATE TABLE game(
 		away_q2_score >= 0 AND
 		away_q3_score >= 0 AND
 		away_q4_score >= 0
-		),
+  ),
 	CHECK (
-	    home_ot_score >= 0 OR
-	    home_ot_score IS NULL
-	    ),
+		home_ot_score >= 0 OR
+		home_ot_score IS NULL
+  ),
 	CHECK (
-	    away_ot_score >= 0 OR
-	    away_ot_score IS NULL
-	    )
+		away_ot_score >= 0 OR
+		away_ot_score IS NULL
+  )
 );
 
 
 CREATE TABLE game_referee (
-	g_id int REFERENCES game (g_id) ON DELETE CASCADE,
+	g_id int REFERENCES game (g_id) ON DELETE NO ACTION,
 	r_id int REFERENCES referee (r_id),
 	PRIMARY KEY (g_id, r_id)
 );
 
 
 CREATE TABLE player_game_stats (
-	g_id int REFERENCES game (g_id) ON DELETE CASCADE,
-	p_id int REFERENCES player (p_id),
+	g_id int REFERENCES game (g_id) ON DELETE NO ACTION,
+	p_id int REFERENCES player (p_id) ON DELETE NO ACTION,
 	PRIMARY KEY (g_id, p_id),
 	t_id int REFERENCES team (t_id),
-	Minutes_played int NOT NULL,
-  	Field_goals_made int NOT NULL,
-  	Field_goal_attempts int NOT NULL, 
-  	Three_pointers_made int NOT NULL,
-  	Three_point_attempts int NOT NULL,
-  	Free_throws_made int NOT NULL,
-  	Free_throw_attempts int NOT NULL,
-  	Offensive_rebounds int NOT NULL,
-  	Defensive_rebounds int NOT NULL,
-  	Assists int NOT NULL,
-  	Steals int NOT NULL,
-  	Blocks int NOT NULL,
-  	Turnovers int NOT NULL,
-  	Personal_fouls int NOT NULL,
-  	Points int NOT NULL,
-  	Plus_minus int NOT NULL,
-  	Offensive_rebound_percentage int NOT NULL,
-  	Defensive_rebound_percentage int NOT NULL,
-  	Total_rebound_percentage int NOT NULL,
-  	Assist_percentage int NOT NULL, 
-  	Steal_percentage int NOT NULL,
-  	Block_percentage int NOT NULL,
-  	Turnover_percentage int NOT NULL,
-  	Usage_percentage int NOT NULL,
-  	Offensive_rating int NOT NULL,
-  	Defensive_rating int NOT NULL,
-  	CHECK (
-  	    Field_goals_made <= Field_goal_attempts AND
-  	    Three_pointers_made <= Three_point_attempts AND
-  	    Free_throws_made <= Free_throw_attempts
-  	    ),
-  	CHECK (
-  	    Minutes_played >= 0 AND
-  	    Field_goals_made >= 0 AND
-  	    Field_goal_attempts >= 0 AND
-  	    Three_pointers_made >= 0 AND
-  	    Three_point_attempts >= 0 AND
-  	    Free_throws_made >= 0 AND
-  	    Free_throw_attempts >= 0 AND
-  	    Offensive_rebounds >= 0 AND
-  	    Defensive_rebounds >= 0 AND
-  	    Steals >= 0 AND
-  	    Blocks >= 0 AND
-  	    Turnovers >= 0 AND
-  	    Personal_fouls >= 0 AND
-  	    Points >= 0 AND
-  	    Offensive_rebound_percentage >= 0 AND
-  	    Defensive_rebound_percentage >= 0 AND
-  	    Total_rebound_percentage >= 0 AND
-  	    Assist_percentage >= 0 AND
-  	    Steal_percentage >= 0 AND
-        Block_percentage >= 0 AND
-  	    Turnover_percentage >= 0 AND
-  	    Usage_percentage >= 0 AND
-  	    Offensive_rating >= 0 AND
-  	    Defensive_rating >= 0
-  	    )
+	minutes_played int NOT NULL,
+	field_goals_made int NOT NULL,
+	field_goal_attempts int NOT NULL,
+	three_pointers_made int NOT NULL,
+	three_point_attempts int NOT NULL,
+  free_throws_made int NOT NULL,
+	free_throw_attempts int NOT NULL,
+	offensive_rebounds int NOT NULL,
+	defensive_rebounds int NOT NULL,
+	assists int NOT NULL,
+	steals int NOT NULL,
+	blocks int NOT NULL,
+	turnovers int NOT NULL,
+	personal_fouls int NOT NULL,
+	points int NOT NULL,
+	plus_minus int NOT NULL,
+	offensive_rebound_percentage int NOT NULL,
+	defensive_rebound_percentage int NOT NULL,
+	total_rebound_percentage int NOT NULL,
+	assist_percentage int NOT NULL,
+	steal_percentage int NOT NULL,
+	block_percentage int NOT NULL,
+	turnover_percentage int NOT NULL,
+	usage_percentage int NOT NULL,
+	offensive_rating int NOT NULL,
+	defensive_rating int NOT NULL,
+	CHECK (
+		field_goals_made <= field_goal_attempts AND
+		three_pointers_made <= three_point_attempts AND
+		free_throws_made <= free_throw_attempts
+	),
+	CHECK (
+		minutes_played >= 0 AND
+		field_goals_made >= 0 AND
+		field_goal_attempts >= 0 AND
+		three_pointers_made >= 0 AND
+		three_point_attempts >= 0 AND
+		free_throws_made >= 0 AND
+		free_throw_attempts >= 0 AND
+		offensive_rebounds >= 0 AND
+		defensive_rebounds >= 0 AND
+		steals >= 0 AND
+		blocks >= 0 AND
+		turnovers >= 0 AND
+		personal_fouls >= 0 AND
+		points >= 0 AND
+		offensive_rebound_percentage >= 0 AND
+		defensive_rebound_percentage >= 0 AND
+		total_rebound_percentage >= 0 AND
+		assist_percentage >= 0 AND
+		steal_percentage >= 0 AND
+		block_percentage >= 0 AND
+		turnover_percentage >= 0 AND
+		usage_percentage >= 0 AND
+		offensive_rating >= 0 AND
+		defensive_rating >= 0
+	)
 );
 
--- sportsbook table
-CREATE TABLE sb_game_odds(
-	g_id int REFERENCES game (g_id) ON DELETE CASCADE,
-	sb_id int REFERENCES sportsbook (sb_id) ON DELETE CASCADE,
-    odds_time timestamp, -- sportsbook will update odds intermittently
-	PRIMARY KEY (g_id, sb_id, odds_time), 
-	h_money_line int,
-	a_money_line int,
-	h_spread int,
-	a_spread int,
-	over_line int,
-	under_line int,
-	spread_value int,
-	over_under_value int
+-- odds lines table
+CREATE TABLE make_odds (
+	g_id int REFERENCES game (g_id) ON DELETE NO ACTION,
+	sb_id int REFERENCES sportsbook (sb_id) ON DELETE NO ACTION,
+	bt_id int REFERENCES bet_type (bt_id) ON DELETE NO ACTION,
+  odds_time timestamp NOT NULL, -- sportsbook will update odds intermittently
+	PRIMARY KEY (g_id, sb_id, bt_id, odds_time),
+	odds_line int NOT NULL,
+  CHECK (odds_line)
 );
+
 
 -- user bet table
 CREATE TABLE place_bet(
-	b_id int PRIMARY KEY,
-	u_id int REFERENCES users (u_id) ON DELETE CASCADE,
-	g_id int REFERENCES game (g_id) ON DELETE CASCADE,
-	sb_id int REFERENCES sportsbook (sb_id) ON DELETE CASCADE,
+	g_id int,
+	sb_id int,
+	bt_id int,
+	odds_time timestamp NOT NULL,
+	FOREIGN KEY (g_id, sb_id, bt_id, odds_time) REFERENCES make_odds(g_id, sb_id, bt_id, odds_time),
 	bet_time timestamp NOT NULL,
-	bet_h_money_line int,
-	bet_a_money_line int,
-	bet_h_spread int,
-	bet_a_spread int,
-	bet_over_line int,
-	bet_under_line int,
-	bet_spread_value int,
-	bet_over_under_value int
+	u_id int REFERENCES users (u_id) ON DELETE NO ACTION,
+	PRIMARY KEY (u_id, g_id, sb_id, bt_id, odds_time, bet_time)
 );
