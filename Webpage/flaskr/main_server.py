db98aa2d4aba268d802c64990932007b587872b0
PGrantcharov <peter.g@columbia.edu>
2019-04-13 23:30:27 -0400

    last push for night, looking good

--- Webpage/flaskr/main_server.py
+++ Webpage/flaskr/main_server.py
@@ -9,7 +9,8 @@ from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import (
 Flask, request, render_template, g,
-redirect, Response, session, url_for
+redirect, Response, session, url_for,
+flash
 )
 import traceback
 import click
@@ -21,6 +22,7 @@ import time
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 print(tmpl_dir)
 app = Flask(__name__, template_folder=tmpl_dir)
+app.secret_key = os.urandom(2)
 
 DATABASEURI = "postgresql://pdg2116:f5ih31DBMB@w4111.cisxo09blonu.us-east-1.rds.amazonaws.com/w4111"
 engine = create_engine(DATABASEURI)
@@ -45,24 +47,26 @@ def teardown_request(exception):
 
 
 @app.route('/')
-def homepage():
+def homepage(bet_msg=None):
     """
     request.method:   "GET" or "POST"
     request.form:     if the browser submitted a form, this contains the data in the form
     request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
     """
-    user_id = session.get('user_id')
-    if user_id is None:
-        statement = None
-        balance = None
+    context = {}
+    if session.get('user_id') is None:
+        context['logged_in'] = False
     else:
-        statement = "SELECT balance FROM users WHERE u_id = " + user_id + ';'
-        balance = db_select(statement).iloc[0, 0]
+        context['logged_in'] = True
+        user_id = session['user_id']
+        statement = "SELECT first_name, balance FROM users WHERE u_id = {id};".format(id=user_id)
+        first, balance = engine.execute(statement).fetchone()
+        context['first_name'] = first
+        context['balance'] = '{:20,.2f}'.format(balance)
 
-    betting_data = get_betting_data()
+    context['betting_data'] = get_betting_data()
 
-    placeholder = {'1':1, '2':2}
-    return render_template("homepage.html", **placeholder)
+    return render_template("homepage.html", **context)
 
 
 @app.route('/user_place_bet', methods=['POST'])
@@ -73,17 +77,15 @@ def user_place_bet():
     # TODO: need to know which site the user came from when placing bet
     amount = request.form['amount']
     o_id = request.form['odds_id']
-    # TODO: implement checking function doing the following:
-    # TODO: check that this is value Amount
-    # TODO: check that user has at least this amount in balance
     error =  valid_amount(amount=amount, bet=o_id)
     if error is not None:
-        statement = "INSERT INTO place_bet (o_id, u_ud, bet_size) VALUES "
-        statement = statement + str((o_id, u_id, amount)) + ';'
+        statement = "INSERT INTO place_bet (o_id, u_ud, bet_size) VALUES {};".format(str((o_id, u_id, amount)))
         g.conn.execute(statement)
-        return redirect('/', result='Successfully placed bet!')
+        bet_result = 'Successfully placed bet!'
     else:
-        return redirect('/', result=error)
+        bet_result = error
+    flash(bet_result)
+    return redirect('/')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -91,21 +93,22 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = engine.execute(
-            'SELECT * FROM user WHERE username = ?',
-            (username,)).fetchone()
+        user = engine.execute('SELECT * FROM users WHERE username = %s;',(username,)).fetchone()
         if user is None:
             error = 'Incorrect username.'
         elif password != user['password']:
             error = 'Incorrect password.'
         else:
             session.clear()
-            session['user_id'] = user['id']
-            return render_template('homepage.html')
-    return render_template('login.html', error=error)
+            session['user_id'] = user['u_id']
+            session['balance'] = user['balance']
+            session['logged_in'] = True
+            return redirect('/')
+        flash(error)
+    return render_template('login.html')
 
 
-@app.route('/register', methods=('GET', 'POST'))
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -117,19 +120,23 @@ def register():
         if None in form_list:
             error = 'Please fill in all fields.'
         else:
-            users = engine.execute('SELECT id FROM user WHERE username = ?',
-                                   (username,)).fetchone()
-
+            user = engine.execute('SELECT id FROM user WHERE username = "%s"', (username,)).fetchone()
             if users is not None:
                 error = 'User {} is already registered.'.format(username)
             else:
                 register_user(form_list)
-                user = engine.execute('SELECT id FROM user WHERE username = ?',
-                                      (username,)).fetchone()
                 session.clear()
                 session['user_id'] = user['id']
                 return render_template('homepage.html')
-        return render_template('register.html', error=error)
+        flash(error)
+    return render_template('register.html')
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.clear()
+    flash("Successfully logged out")
+    return render_template('login.html')
 
 
 """
@@ -142,9 +149,17 @@ def register_user(form_list):
         VALUES (?, ?, ?, ?)""", tuple(form_list)
     )
 
-def valid_amount(amount, bet):
-    # TODO: fill this function
-    pass
+def valid_amount(amount):
+    # verify input
+    money = re.compile('|'.join([r'^\$?(\d*\.\d{1,2})$', r'^\$?(\d+)$', r'^\$(\d+\.?)$']))
+    try:
+        amount = [x for x in filter(None, money.match(amount).groups())][0]
+    except AttributeError:
+        return "Incorrect Input"
+
+    # verify sufficient funds
+    if amount > session.get('balance'):
+        return "Insufficient Funds"
 
 
 def db_select(statement):
@@ -155,14 +170,14 @@ def get_betting_data():
     lower = (dt.datetime.now() - dt.timedelta(days=1)).strftime('%Y-%m-%d')
     upper = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     bounds = """game_time > '""" + lower + "' AND game_time < '" + upper
-    games_statement = """SELECT * FROM game WHERE """ + bounds + "';"
+    games_statement = """SELECT * FROM game WHERE {b}';""".format(b=bounds)
     bet_statement = """
     SELECT *
     FROM make_odds
     WHERE make_odds.g_id in (
         SELECT game.g_id
         FROM game
-        WHERE """ + bounds + "');"
+        WHERE {b}');""".format(b=bounds)
     return clean_display_data(db_select(games_statement), db_select(bet_statement))
 
 
@@ -171,56 +186,32 @@ def clean_display_data(game_df, bet_df):
     teams = dict(zip(teams.t_id, teams.name))
     game_df[['t_id_home', 't_id_away']] = game_df.iloc[:, -2:].applymap(lambda x: teams[x])
 
-    game_times = game_df.game_times.values
+    game_times = game_df.game_time.map(lambda x: dt.datetime.strftime(x, '%c')).values
     away_team = game_df.t_id_away
     home_team = game_df.t_id_home
 
-    away_ml = [get_best_bet(x.g_id, 'ml', 'V') for x in game_df.g_id.values]
-    home_ml = [get_best_bet(x.g_id, 'ml', 'H') for x in game_df.g_id.values]
-    away_ps = [get_best_bet(x.g_id, 'ps', 'V') for x in game_df.g_id.values]
-    home_ps = [get_best_bet(x.g_id, 'ps', 'H') for x in game_df.g_id.values]
-    over = [get_best_bet(x.g_id, 'ou', 'O') for x in game_df.g_id.values]
-    under = [get_best_bet(x.g_id, 'ou', 'U') for x in game_df.g_id.values]
+    away_ml = [get_best_bet(x, bt_id=1, side='V') for x in game_df.g_id.values]
+    home_ml = [get_best_bet(x, bt_id=1, side='H') for x in game_df.g_id.values]
+    away_ps = [get_best_bet(x, bt_id=3, side='V') for x in game_df.g_id.values]
+    home_ps = [get_best_bet(x, bt_id=3, side='H') for x in game_df.g_id.values]
+    over = [get_best_bet(x, bt_id=2, side='O') for x in game_df.g_id.values]
+    under = [get_best_bet(x, bt_id=2, side='U') for x in game_df.g_id.values]
+    return zip(game_times, away_team, home_team, away_ml, home_ml, away_ps, home_ps, over, under)
     # Link to deeper page
-"""
-CREATE TABLE make_odds (
-	o_id int SERIAL PRIMARY KEY,
-	g_id VARCHAR (12) REFERENCES game (g_id),
-	sb_id int REFERENCES sportsbook (sb_id) ON DELETE NO ACTION,
-	bt_id int REFERENCES bet_type (bt_id) ON DELETE NO ACTION,
-	odds_time timestamp NOT NULL,
-	odds_side char NOT NULL,
-	CHECK (
-		odds_side = 'H' OR
-		odds_side = 'V' OR
-		odds_side = 'O' OR
-		odds_side = 'U'
-	),
-	UNIQUE (g_id, sb_id, bt_id, odds_time, odds_side),
-	odds_payout float NOT NULL,
-	CHECK (odds_payout >= 1),
-	odds_line float NOT NULL
-);
-"""
 
-# for moneyline, just want max payout
-# for spread, want max odds odds_line for side
-# for over under, want min oddsline if over, max if under
 
-def get_best_bet(game_id, kind, side):
-    bet_id = 1 if kind == 'ml' else (2 if kind == 'ou' else 3)
-    dir = 'ASC' if kind == 'ou' and side == 'O' else 'DESC'
-    """
-    SELECT *
+def get_best_bet(game_id, bt_id, side):
+    dir = 'ASC' if bt_id == 2 and side == 'O' else 'DESC'
+    statement = """
+    SELECT odds_line, odds_payout
     FROM make_odds as m
-    WHERE m.g_id = {g_id}
+    WHERE m.g_id = '{g_id}'
     AND m.bt_id = {bt_id}
-    AND m.side = {side}
-    ORDER BY m.odds_line {dir}, m.odds_payout DESC
-    """.format(g_id=game_id, bt_id=bet_id, side=side, dir=dir)
+    AND m.odds_side = '{side}'
+    ORDER BY m.odds_line {dir}, m.odds_payout DESC;
+    """.format(g_id=game_id, bt_id=bt_id, side=side, dir=dir)
+    return engine.execute(statement).fetchone()
 
-    # REMEMBER TO ONLY FETCHONE
-    pass
 
 if __name__ == "__main__":
   @click.command()
