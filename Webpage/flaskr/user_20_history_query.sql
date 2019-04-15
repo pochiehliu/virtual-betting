a5c2bb168030c2b4fee041f6beb3e2f52f50ef7c
Po <42683302+pochiehliu@users.noreply.github.com>
2019-04-14 21:38:28 -0400

    Update user_20_history_query.sql

--- Webpage/flaskr/user_20_history_query.sql
+++ Webpage/flaskr/user_20_history_query.sql
@@ -8,10 +8,12 @@
 -- 7) results = {WIN, LOSS,  PENDING}
 
 %%sql
+%%sql
 WITH bet_info AS(
-    SELECT P.bet_time, M.o_id, M.g_id, M.odds_side, M.odds_payout, M.odds_line, P.bet_size
-    FROM place_bet AS P, make_odds AS M
-    WHERE P.o_id = M.o_id AND P.u_id = 1
+    SELECT P.bet_time, M.o_id, M.g_id, M.odds_side, M.odds_payout, M.odds_line, 
+           P.bet_size, BT.name AS BET_TYPE
+    FROM place_bet AS P, make_odds AS M, bet_type AS BT
+    WHERE P.o_id = M.o_id AND P.u_id = 1 AND BT.bt_id = M.bt_id
     ORDER BY bet_time DESC
     LIMIT 20),
 team_info AS(
@@ -29,14 +31,16 @@ game_info AS(
     FROM game_stats AS GS
     WHERE GS.g_id IN (SELECT g_id FROM bet_info)),
 OU AS (
-    SELECT B.bet_time, T.HOME, T.AWAY, G.H AS H_score, G.V AS V_score, B.bet_size,
+    SELECT B.bet_time, T.HOME, T.AWAY, G.H AS H_score, G.V AS V_score, B.bet_size, 
+           B.BET_TYPE, B.odds_side,
            CASE WHEN (G.OU > B.odds_line AND B.odds_side = 'O') = TRUE THEN 'WIN'
                 ELSE 'LOST' END AS WIN_LOST
     FROM bet_info AS B, game_info AS G, team_info AS T
     WHERE B.g_id = G.g_id AND (B.odds_side = 'O' OR B.odds_side = 'U') AND B.o_id = T.o_id
 ),
 HV AS(
-    SELECT B.bet_time, T.HOME, T.AWAY, G.H AS H_score, G.V AS V_score, B.bet_size,
+    SELECT B.bet_time, T.HOME, T.AWAY, G.H AS H_score, G.V AS V_score, B.bet_size, 
+           B.BET_TYPE, B.odds_side,
            CASE WHEN (G.H-G.V > B.odds_line AND B.odds_side = 'H') = TRUE THEN 'WIN'
                 ELSE 'LOST' END AS WIN_LOST
     FROM bet_info AS B, game_info AS G, team_info AS T
@@ -44,7 +48,7 @@ HV AS(
 ),
 INCOMPLETE AS (
     SELECT B.bet_time, T.HOME, T.AWAY, 0 AS H_score, 0 AS V_score, B.bet_size, 
-           'PENDING' AS WIN_LOST
+           B.BET_TYPE, B.odds_side, 'PENDING' AS WIN_LOST
     FROM bet_info AS B, team_info AS T
     WHERE B.g_id NOT IN (SELECT g_id FROM game_info) AND B.o_id = T.o_id
 )
