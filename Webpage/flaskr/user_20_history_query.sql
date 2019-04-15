1a85d689bf835f0be7ce7c63e2ed59fc8bf27aad
Po <42683302+pochiehliu@users.noreply.github.com>
2019-04-14 22:18:27 -0400

    Update user_20_history_query.sql

--- Webpage/flaskr/user_20_history_query.sql
+++ Webpage/flaskr/user_20_history_query.sql
@@ -13,7 +13,7 @@ WITH bet_info AS(
     FROM place_bet AS P, make_odds AS M, bet_type AS BT
     WHERE P.o_id = M.o_id AND P.u_id = 1 AND BT.bt_id = M.bt_id
     ORDER BY bet_time DESC
-    LIMIT 20),
+    LIMIT 50),
 team_info AS(
     SELECT B.o_id, T1.name AS Home, T2.name AS AWAY
     FROM bet_info AS B, game AS G, team AS T1, team AS T2
@@ -29,24 +29,29 @@ game_info AS(
     FROM game_stats AS GS
     WHERE GS.g_id IN (SELECT g_id FROM bet_info)),
 OU AS (
-    SELECT B.bet_time, T.HOME, T.AWAY, G.H AS H_score, G.V AS V_score, B.bet_size, 
-           B.BET_TYPE, B.odds_line, B.odds_side,
-           CASE WHEN (G.OU > B.odds_line AND B.odds_side = 'O') = TRUE THEN 'WIN'
+    SELECT B.bet_time, T.HOME, T.AWAY, G.H AS H_score, G.V AS V_score, B.bet_size, B.odds_side, 
+           B.BET_TYPE, B.odds_line, B.odds_payout,
+           CASE WHEN ((G.OU > B.odds_line AND B.odds_side = 'O') OR
+                      (G.OU < B.odds_line AND B.odds_side = 'V')) = TRUE THEN 'WIN'
+                WHEN (G.OU = B.odds_line) THEN 'TIE'
                 ELSE 'LOST' END AS WIN_LOST
     FROM bet_info AS B, game_info AS G, team_info AS T
     WHERE B.g_id = G.g_id AND (B.odds_side = 'O' OR B.odds_side = 'U') AND B.o_id = T.o_id
 ),
 HV AS(
-    SELECT B.bet_time, T.HOME, T.AWAY, G.H AS H_score, G.V AS V_score, B.bet_size, 
-           B.BET_TYPE, B.odds_line, B.odds_side,
-           CASE WHEN (G.H-G.V > B.odds_line AND B.odds_side = 'H') = TRUE THEN 'WIN'
+    SELECT B.bet_time, T.HOME, T.AWAY, G.H AS H_score, G.V AS V_score, B.bet_size, B.odds_side,
+           B.BET_TYPE, B.odds_line, B.odds_payout,
+           CASE WHEN (((G.H-G.V + B.odds_line)>0 AND B.odds_side = 'H') OR
+                      ((G.H-G.V - B.odds_line)<0 AND B.odds_side = 'V')) = TRUE THEN 'WIN'
+                WHEN (((G.H-G.V + B.odds_line)=0 AND B.odds_side = 'H' AND B.BET_TYPE = 'spread') OR
+                      ((G.H-G.V - B.odds_line)=0 AND B.odds_side = 'V' AND B.BET_TYPE = 'spread')) = TRUE THEN 'TIE'
                 ELSE 'LOST' END AS WIN_LOST
     FROM bet_info AS B, game_info AS G, team_info AS T
     WHERE B.g_id = G.g_id AND (B.odds_side = 'H' OR B.odds_side = 'V') AND B.o_id = T.o_id
 ),
 INCOMPLETE AS (
-    SELECT B.bet_time, T.HOME, T.AWAY, 0 AS H_score, 0 AS V_score, B.bet_size, 
-           B.BET_TYPE, B.odds_line, B.odds_side, 'PENDING' AS WIN_LOST
+    SELECT B.bet_time, T.HOME, T.AWAY, 0 AS H_score, 0 AS V_score, B.bet_size, B.odds_side,
+           B.BET_TYPE, B.odds_line, B.odds_payout, 'PENDING' AS WIN_LOST
     FROM bet_info AS B, team_info AS T
     WHERE B.g_id NOT IN (SELECT g_id FROM game_info) AND B.o_id = T.o_id
 )
